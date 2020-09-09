02c3ccb051ddf263e083aa80093e3eb57be00c47
giri-sai <girim28121992@gmail.com>
2020-09-09 11:25:37 +0530

    'mschdnge'

--- Testpage.py
+++ Testpage.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 #from selenium.webdriver.common.exceptions import
 import time
+driver1 = webdriver.Firefox()
 driver = webdriver.Chrome(executable_path = "C:\sai\ChromeDriver\chromedriver.exe")
 driver.get("http://www.flipkart.com")
 time.sleep(5)
